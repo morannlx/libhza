e7e7d8d4e606e6ae71371e1e0e9dea9e58f934a4
morannlx <2152533805@qq.com>
2024-09-01 10:29:02 +0800

    magisksu

--- core/src/main/java/com/topjohnwu/superuser/internal/BuilderImpl.java
+++ core/src/main/java/com/topjohnwu/superuser/internal/BuilderImpl.java
@@ -85,7 +85,7 @@ public final class BuilderImpl extends Shell.Builder {
         // Root mount master
         if (!hasFlags(FLAG_NON_ROOT_SHELL) && hasFlags(FLAG_MOUNT_MASTER)) {
             try {
-                shell = exec("hza", "--mount-master");
+                shell = exec("magisk su", "--mount-master");
                 if (!shell.isRoot())
                     shell = null;
             } catch (NoShellException ignore) {}
@@ -94,7 +94,7 @@ public final class BuilderImpl extends Shell.Builder {
         // Normal root shell
         if (shell == null && !hasFlags(FLAG_NON_ROOT_SHELL)) {
             try {
-                shell = exec("hza");
+                shell = exec("magisk su");
                 if (!shell.isRoot()) {
                     shell = null;
                 }
