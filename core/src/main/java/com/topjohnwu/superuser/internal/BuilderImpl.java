103876d5e88dc45735651c12cf38ac4363d515d4
墨染_nlx <78503566+morannlx@users.noreply.github.com>
2024-11-06 10:41:45 +0800

    Update BuilderImpl.java
    timesu

--- core/src/main/java/com/topjohnwu/superuser/internal/BuilderImpl.java
+++ core/src/main/java/com/topjohnwu/superuser/internal/BuilderImpl.java
@@ -85,7 +85,7 @@ public final class BuilderImpl extends Shell.Builder {
         // Root mount master
         if (!hasFlags(FLAG_NON_ROOT_SHELL) && hasFlags(FLAG_MOUNT_MASTER)) {
             try {
-                shell = exec("magisk su", "--mount-master");
+                shell = exec("timesu", "--mount-master");
                 if (!shell.isRoot())
                     shell = null;
             } catch (NoShellException ignore) {}
@@ -94,7 +94,7 @@ public final class BuilderImpl extends Shell.Builder {
         // Normal root shell
         if (shell == null && !hasFlags(FLAG_NON_ROOT_SHELL)) {
             try {
-                shell = exec("magisk su");
+                shell = exec("timesu");
                 if (!shell.isRoot()) {
                     shell = null;
                 }
